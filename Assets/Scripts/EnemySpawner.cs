96f531bcc1957c1bda84f96726df7302c125e699
Anthony-Coelho <91150089+Anthony-Coelho@users.noreply.github.com>
2026-04-03 17:22:29 -0400

    Enemies and spawner
    The spawner works, but the enemies have no logic

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+using UnityEditor.Animations;
+using UnityEngine;
+
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemies;
+    [SerializeField] private float spawnTimer = 10f;
+    [SerializeField] private int maxEnemies = 3;
+    private int activeEnemies = 0;
+    private float timer = 0f;
+    private int randomEnemy;
+
+    void Start()
+    {
+        int seed = System.DateTime.Now.Millisecond + gameObject.GetInstanceID();
+        Random.InitState(seed);
+    }   
+    void Update()
+    {
+        if (activeEnemies != maxEnemies)
+        {
+            timer += Time.deltaTime;
+            if (timer >= spawnTimer)
+            {
+                SpawnEnemy();
+                timer = 0f;
+            }
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        randomEnemy = Random.Range(0, enemies.Length);
+        GameObject enemy = Instantiate(enemies[randomEnemy], transform.position, Quaternion.identity);
+        activeEnemies++;
+    }
+}
